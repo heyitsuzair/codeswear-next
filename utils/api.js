2adcc94fc248391301cc78cd5a7a2969dfe4086b
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-11-19 05:24:53 +0500

    Tshirt Dynamic

--- utils/api.js
+++ utils/api.js
@@ -1,2 +1,4 @@
 export const host = "http://localhost:3000/";
+export const backend = "http://localhost:5000/";
 export const fetchPincodes = host + "api/pincode";
+export const getProducts = backend + "api/product/";
